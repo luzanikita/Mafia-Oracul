6f916e6635340946a7660723ec69a3bc215587cd
nigi4 <luzanikita@gmail.com>
2018-07-01 19:09:13 +0300

    Read data from all clubs and leagues

--- scrapper.py
+++ scrapper.py
@@ -3,33 +3,77 @@ import urllib.request
 from bs4 import BeautifulSoup
 from player import Player
 
-def get_url(url):
+URL = "https://new.the-mafia.net"
+
+def get_html(url):
     response = urllib.request.urlopen(url)
     return response.read()
 
-def parse(html):    
-    players = {}
+def get_clubs(url):
+    clubs = []
+    html = get_html(url)
+    soup = BeautifulSoup(html, "lxml")
+
+    links = soup.find_all("div", class_="tournament-info-top")
+    for link in links:
+        clubs.append(URL + link.find("a").get("href"))
+
+    if soup.find("li", class_="pager-next") is None:
+        return clubs
+    
+    return clubs + get_clubs(URL +
+        soup.find("li", class_="pager-next")\
+            .find("a")\
+            .get("href"))
+
+def get_leagues(club_url):
+    leagues = []
+    html = get_html(club_url)
+    soup = BeautifulSoup(html, "lxml")
+    links = soup.find("ul", class_="links-list")
+        
+    if links != None:
+        for link in links.find_all("a"):
+            leagues.append(URL +
+                link.get("href"))
+
+    return leagues
+
+def get_players_info(league_url, players={}):
+    html = get_html(league_url)
     soup = BeautifulSoup(html, "lxml")
     table = soup.find("table", class_="tournament-table bordered")
+    
+    if table != None:
+        for row in table.find_all("tr")[1:]:
+            player_info = []
 
-    for row in table.find_all("tr")[1:]:
-        player_info = []
+            for column in row.find_all("td")[1:]:
+                player_info.append(column.text.strip())
 
-        for column in row.find_all("td")[1:]:
-            player_info.append(column.text.strip())
-
-        player = Player(*player_info)
-        if player.name in players.keys():
-            players[player.name].merge(player)
-        else:
-            players[player.name] = player
+            player = Player(*player_info)
+            if player.name in players.keys():
+                players[player.name].merge(player)
+            else:
+                players[player.name] = player
 
     return players
 
 def main():
-    players = parse(get_url("https://new.the-mafia.net/?q=rate/sezon-2018-vtoraya-liga"))
-    print(players)
-    #https://new.the-mafia.net/?q=rate/sezon-2018-vtoraya-liga
+    clubs = get_clubs(URL + "/?q=clubs_list")
+    players = {}
+
+    for club in clubs:
+        leagues = get_leagues(club)
+        
+        for player in leagues:
+            try:
+                players = get_players_info(player, players)
+            except:
+                pass
+        print(club)
+
+    print("Done!")
 
 if __name__ == "__main__":
     main()
