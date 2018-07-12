c675cf9c9d80facfb0293f1785ad6ae97f21e1b0
nigi4 <luzanikita@gmail.com>
2018-07-12 19:05:02 +0300

    - Minor changes in project architecture (sorted files by folders) - Added data normalition

---
+++ Preparation/data_normalization.py
@@ -0,0 +1,24 @@
+import pandas as pd
+
+def to_normal(series):
+    min_ = series.min()
+    max_ = series.max()
+    series = (series - min_) / (max_ - min_)
+    
+    return series
+
+def main():
+    players_stats = pd.read_csv('../Data/stats.csv')
+
+    normal_stats = players_stats\
+        .iloc[:,2:]\
+        .transform(to_normal)  
+    
+    players_stats = pd.concat(
+        [players_stats['Name'], normal_stats],
+        axis=1)
+
+    players_stats.to_csv('../Data/normal_stats.csv')
+
+if __name__ == '__main__':
+    main()
