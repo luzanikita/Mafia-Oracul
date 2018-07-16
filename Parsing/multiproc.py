5093d7f4d3c7e376cc35b0d4dda84421daa4c6f2
nigi4 <luzanikita@gmail.com>
2018-07-16 22:54:43 +0300

    - Add player's position to game info - First realisation of multiprocessing

--- Parsing/multiproc.py
+++ Parsing/multiproc.py
@@ -1,6 +1,26 @@
+import games_parser as gp
+import pandas as pd
+from multiprocessing import Pool
+
 URL = 'https://the-mafia.net'
 
 def get_links(link):
     return URL + link.find('a').get('href')
 
-def get_href(a):
+def map_games(game):
+    return gp.get_game_info(game)
+
+def map_leagues(league):
+    try:
+        games_url = gp.get_games_url(league)
+        
+        if games_url != None:
+            return gp.get_games(games_url)
+        else:
+            return []
+    
+    except:
+        return []
+
+def map_clubs(club):
+    return gp.get_leagues(club)
