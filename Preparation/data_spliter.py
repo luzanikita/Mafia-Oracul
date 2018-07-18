45769216bd7400c0f2ae68dc2f7b25ea373d9ed6
nigi4 <luzanikita@gmail.com>
2018-07-18 15:04:39 +0300

    - Merged games info and players' stats (almost)

--- Preparation/data_spliter.py
+++ Preparation/data_spliter.py
@@ -1,9 +1,29 @@
 import pandas as pd
 import numpy as np
+import ast
 
-def make_set(players, games):
-    data_set = pd.DataFrame()
+def make_set(players, game):
+    citizens = []
+    mafia = []
 
-players = pd.read_csv('Data/normal_stats.csv')
-games = pd.read_csv('Data/games.csv')
+    for i in ast.literal_eval(game.at['Citizen']):
+        info = players.ix[players['Name'] == i[1], 1:].as_matrix().flatten()
+        info = np.append(info, i[0] / 10.)
+        citizens.append(info)
 
+    for i in ast.literal_eval(game.at['Mafia']):
+        info = players.ix[players['Name'] == i[1], 1:].as_matrix().flatten()
+        info = np.append(info, i[0] / 10.)
+        mafia.append(info)
+
+    return citizens, mafia
+
+def main():
+    players = pd.read_csv('Data/normal_stats.csv')
+    games = pd.read_csv('Data/games.csv')
+    players = players.iloc[:,1:]
+
+    print(make_set(players, games.iloc[0]))
+
+if __name__ == '__main__':
+    main()
